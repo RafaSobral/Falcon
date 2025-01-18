18041c8641595ff111a2e6f142a93b0e5ef9b35d
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-01-18 10:40:57 -0300

    exercicio6

--- exercicio5.py
+++ exercicio5.py
@@ -1,15 +1,19 @@
 class Pessoa:
+    '''Classe para representar uma pessoa com nome, idade e endereco'''
     def __init__(self, nome, idade, endereco):
+        '''atributos da pessoa'''
         self.nome = nome
         self.idade = int(idade)
         self.endereco = endereco
 
     def exibir_dados(self):
+        '''exibe os dados da pessoa'''
         print('Nome: ' + self.nome)
         print('Idade: ' + str(self.idade))
         print('Endereco: ' + self.endereco)
 
     def set_dados(self, valor):
+        '''atualiza o endereco da pessoa'''
         self.endereco = valor
         print('endereco atualizado')
 
