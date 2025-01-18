267400aad21b6d32cd54078575191c02a2b7dcdd
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2025-01-18 13:11:25 -0300

    correcao exercicio9

--- exercicio9.py
+++ exercicio9.py
@@ -10,15 +10,18 @@ class Gerente(Funcionario):
 
     def calcular_salario(self):
         self.total = self.salario + self.bonus 
-        print(f'Total: {self.total}')
+        print(self.total)
     
     def exibir_dados(self):
-        print(f'Nome: {self.nome}\nSalario: {self.salario}\nBonus:{self.bonus}')
+        print(f'Nome: {self.nome}\nSalario: {self.salario}\nBonus:{self.bonus}\nTotal: {self.total}')
 
+func = Funcionario("João", 2000)
+print(func.nome, func.salario)  # João, 2000
 
-func1 = Gerente('Rafael',2232.50,500)
-func1.exibir_dados()
-func1.calcular_salario()
+gerente = Gerente("Maria", 3000, 1500)
+print(gerente.calcular_salario())  # 4500
+gerente.exibir_dados()  # Nome: Maria, Salário: 3000, Bônus: 1500, Total: 4500
+
 
 
         
